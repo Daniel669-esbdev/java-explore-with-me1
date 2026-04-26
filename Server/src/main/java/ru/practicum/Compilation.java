0627b21c68f28494b1d5ec94145b5b7266be05ab
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-04-26 19:16:16 +0500

    1commit

--- Server/src/main/java/ru/practicum/Compilation.java
+++ Server/src/main/java/ru/practicum/Compilation.java
@@ -9,7 +9,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
