b2b8b442ce60dd9fffe5378918e8bfd3d9425b16
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-05-12 23:43:28 +0500

    main_svc

--- ewm-main-service/src/main/java/ru/practicum/model/ParticipationRequest.java
+++ ewm-main-service/src/main/java/ru/practicum/model/ParticipationRequest.java
@@ -1,6 +1,8 @@
 package ru.practicum.model;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -21,10 +23,14 @@ public class ParticipationRequest {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id", nullable = false)
+    @SuppressFBWarnings(value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
+            justification = "Entity references are managed by Hibernate")
     private Event event;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "requester_id", nullable = false)
+    @SuppressFBWarnings(value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"},
+            justification = "Entity references are managed by Hibernate")
     private User requester;
 
     @Enumerated(EnumType.STRING)
