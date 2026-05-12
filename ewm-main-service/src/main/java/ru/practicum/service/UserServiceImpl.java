98d0578f7533aef3157c9696dd97c7c58acd5f34
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-05-12 19:17:09 +0500

    main_svc

---
+++ ewm-main-service/src/main/java/ru/practicum/service/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package ru.practicum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.dto.NewUserRequest;
+import ru.practicum.dto.UserDto;
+import ru.practicum.model.User;
+import ru.practicum.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDto addUser(NewUserRequest newUserRequest) {
+        User user = User.builder()
+                .name(newUserRequest.getName())
+                .email(newUserRequest.getEmail())
+                .build();
+        User savedUser = userRepository.save(user);
+        return toUserDto(savedUser);
+    }
+
+    @Override
+    public List<UserDto> getUsers(List<Long> ids, int from, int size) {
+        PageRequest page = PageRequest.of(from / size, size);
+        List<User> users;
+        if (ids == null || ids.isEmpty()) {
+            users = userRepository.findAll(page).getContent();
+        } else {
+            users = userRepository.findAllByIdIn(ids, page);
+        }
+        return users.stream().map(this::toUserDto).collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    private UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+}
