8d443d76a538853f755476cc1c2fba48b0aa212a
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-05-15 00:05:58 +0500

    main_svc

--- stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -43,8 +43,7 @@ public class StatsClient {
         restTemplate.postForLocation("/hit", hitDto);
     }
 
-    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        Map<String, Object> parameters = Map.of(
+    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {        Map<String, Object> parameters = Map.of(
                 "start", start.format(FORMATTER),
                 "end", end.format(FORMATTER),
                 "uris", String.join(",", uris),
