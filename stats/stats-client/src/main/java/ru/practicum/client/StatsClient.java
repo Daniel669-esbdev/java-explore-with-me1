a8d6fe492d49124c66ddbef634fd60fe5cd171f2
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-05-15 00:29:24 +0500

    main_svc

--- stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -8,13 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class StatsClient {
@@ -23,7 +23,8 @@ public class StatsClient {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private static final ParameterizedTypeReference<List<ViewStatsDto>> STATS_TYPE_REFERENCE =
-            new ParameterizedTypeReference<List<ViewStatsDto>>() {};
+            new ParameterizedTypeReference<List<ViewStatsDto>>() {
+            };
 
     public StatsClient(@Value("${stats-server.url:http://localhost:9090}") String serverUrl,
                        RestTemplateBuilder builder) {
@@ -44,19 +45,19 @@ public class StatsClient {
     }
 
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        Map<String, Object> parameters = Map.of(
-                "start", start.format(FORMATTER),
-                "end", end.format(FORMATTER),
-                "uris", String.join(",", uris),
-                "unique", unique
-        );
+        String url = UriComponentsBuilder.fromUriString("/stats")
+                .queryParam("start", start.format(FORMATTER))
+                .queryParam("end", end.format(FORMATTER))
+                .queryParam("uris", uris)
+                .queryParam("unique", unique)
+                .build()
+                .toUriString();
 
         ResponseEntity<List<ViewStatsDto>> response = restTemplate.exchange(
-                "/stats?start={start}&end={end}&uris={uris}&unique={unique}",
+                url,
                 HttpMethod.GET,
                 null,
-                STATS_TYPE_REFERENCE,
-                parameters
+                STATS_TYPE_REFERENCE
         );
 
         return response.getBody();
