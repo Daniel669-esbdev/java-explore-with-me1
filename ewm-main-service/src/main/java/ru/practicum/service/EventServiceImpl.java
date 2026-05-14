f4a2ffc1479426ef33b2f2c2ab9c94330de2955a
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-05-15 01:31:09 +0500

    main_svc

--- ewm-main-service/src/main/java/ru/practicum/service/EventServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/service/EventServiceImpl.java
@@ -200,17 +200,18 @@ public class EventServiceImpl implements EventService {
         try {
             log.info("Fetching stats for eventId={}", id);
 
-            LocalDateTime start = event.getPublishedOn() != null ? event.getPublishedOn() : event.getCreatedOn();
+            LocalDateTime start = event.getCreatedOn().minusSeconds(1);
+            LocalDateTime end = LocalDateTime.now().plusSeconds(10);
 
             List<ViewStatsDto> stats = statsClient.getStats(
-                    start.minusSeconds(1),
-                    LocalDateTime.now().plusSeconds(1),
+                    start,
+                    end,
                     List.of(request.getRequestURI()),
                     true);
 
             long views = (stats != null && !stats.isEmpty()) ? stats.get(0).getHits() : 0L;
             dto.setViews(views);
-            log.info("EventId={} views updated to: {}", id, views);
+            log.info("EventId={} views updated to: {}, range: [{} - {}]", id, views, start, end);
         } catch (Exception e) {
             log.warn("Failed to get stats for event id={}: {}", id, e.getMessage());
             dto.setViews(0L);
