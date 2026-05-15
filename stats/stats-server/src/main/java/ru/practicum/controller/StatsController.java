c9805b9c9b2d262dafdcc4dc4929fdbf9eb84452
Daniel669-esbdev <daniils.dz@yandex.kz>
2026-05-15 15:03:48 +0500

    main_svc

--- stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,6 +1,7 @@
 package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +15,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class StatsController {
     private final StatsService statsService;
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveHit(@Valid @RequestBody EndpointHitDto hitDto) {
+        log.info("Received hit request: {}", hitDto);
         statsService.saveHit(hitDto);
     }
 
@@ -29,6 +32,14 @@ public class StatsController {
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
             @RequestParam(required = false) List<String> uris,
             @RequestParam(defaultValue = "false") boolean unique) {
+
+        log.info("Received stats request: start={}, end={}, uris={}, unique={}", start, end, uris, unique);
+
+        if (start.isAfter(end)) {
+            log.warn("Invalid date range: start is after end");
+            throw new IllegalArgumentException("Start time must be before end time");
+        }
+
         return statsService.getStats(start, end, uris, unique);
     }
 }
